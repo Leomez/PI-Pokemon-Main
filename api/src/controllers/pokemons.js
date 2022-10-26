602ce0df744ee725d51c6b5141ccfe3756c95b5e
Leonardo Meza <leoariel17@gmail.com>
2022-10-26 15:59:42 -0300

    controller pokemons

--- api/src/controllers/pokemons.js
+++ api/src/controllers/pokemons.js
@@ -1,36 +1,56 @@
 const pokemons_base = require('../models/Pokemon')
-const axios = require('axios')
+const axios = require('axios');
+
 
 const getPokemonsApi = async () => {
-        const url = await axios.get('https://pokeapi.co/api/v2/pokemon');
-        const data = url.data.results.map(e => {
-            return {
-                name: e.name,
-                info: e.url
-            }
-            // return async () => {
-            //     const pokemonUrl = await axios.get(e.url)
-            //     // const pokemonInfo = await pokemonUrl
-            //     return pokemonUrl}
-            })
+    const url = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=5&limit=5');
+    const data = await url.data.results.map(e => {
+        return {
+            name: e.name,
+            info: e.url
+        }
+    })
+    const pokeInfo = data.map(e => axios(e.info))
 
-        console.log(data.info);//no vas a poser xq data es un arreglo, no cada objeto
-        return data;
-    }
+    const info = Promise.all(pokeInfo)
+        .then(res => res.map(el => {
+            
+            let pokemon = {
+                img: el.data.sprites.other.home.front_default,
+                name: el.data.name,
+                id: el.data.id,
+                types: el.data.types,
+                height: el.data.height,
+                stats: el.data.stats,
+                weight: el.data.weight
+            }   
+            console.log(pokemon);
+            return pokemon
+        }
+        ))
+        return info
+}
 
-// const pokeInfo = async () => {    
-
-//         const url = await axios.get(getPokemonsApi.info)
-//         const data = url.data
-//         console.log(data)
-//     }
-
-
-    // get_con_f: async () => {
-    //     const res = await fetch('https://pokeapi.co/api/v2/pokemon');
-    //     const data = await res.json();
-    //     return data
-    // },
-
-// console.log(controller.getPokemonsApi())
 console.log(getPokemonsApi())
+
+
+// const { data: {
+            //     sprites: { other: { home: { front_default } } },
+            //     name,
+            //     id,
+            //     types,
+            //     heigth,
+            //     stats,
+            //     weight
+            // }} = el;
+            // const pokemon = {
+            //     img: front_default,
+            //     name,
+            //     id,
+            //     types,
+            //     heigth,
+            //     stats,
+            //     weight
+            // }
+
+
